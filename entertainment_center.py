94f4157723a7b3d9bc29b9e672ed0303dffb0232
Shawna Jean <shawnajean@users.noreply.github.com>
2017-10-06 23:21:56 -0400

    implement API access to movie list

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,9 @@
-﻿import media
+import media
+import movie_api
+import config
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                       "A story of a boy and his toys that come to life.",
-                       "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                       "https://www.youtube.com/watch?v=vwyZH85NQC4")
+movies = []
+movie_api.get_movie_list(config.themoviedb_list_id, movies)
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet.",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=a0CDJZu4M5I")
-
-your_sisters_sister = media.Movie("Your Sister's Sister",
-                                "Iris invites her friend Jack to stay at her family's island getaway after the death of his brother. At their remote cabin, Jack's drunken encounter with Hannah, Iris' sister, kicks off a revealing stretch of days.",
-                                "https://upload.wikimedia.org/wikipedia/en/6/67/Your_Sister%27s_Sister_poster.jpg",
-                                "https://www.youtube.com/watch?v=l0mmrBfIc4s")
-
-ratatouille = media.Movie("Ratatouille",
-                          "A rat who can cook makes an unusual alliance with a young kitchen worker at a famous restaurant.",
-                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
-
-midnight_in_paris = media.Movie("Midnight in Paris",
-                          "While on a trip to Paris with his fiancée's family, a nostalgic screenwriter finds himself mysteriously going back to the 1920s everyday at midnight.",
-                          "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                          "https://www.youtube.com/watch?v=FAfR8omt-CY")
-
-hunger_games = media.Movie("The Hunger Games",
-                          "Katniss Everdeen voluntarily takes her younger sister's place in the Hunger Games: a televised competition in which two teenagers from each of the twelve Districts of Panem are chosen at random to fight to the death.",
-                          "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                          "https://www.youtube.com/watch?v=mfmrPu43DF8")
-
-movies = [toy_story, avatar, your_sisters_sister, ratatouille, midnight_in_paris, hunger_games]
-fresh_tomatoes.open_movies_page(movies)
-
+print(movies)
---
+++ movie_api.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import config
+import media
+
+
+def get_trailer(movie_id):
+    """
+    Fetches the movie's YouTube trailer key
+
+    Attributes:
+        movie_id (str or int):
+            ID from TheMovieDB.org of the movie to get the trailer of
+    """
+
+    trailer_access_url = ("http://api.themoviedb.org/3/movie/{movie}/videos?"
+                          "api_key={api_key}").format(
+                              movie=str(movie_id),
+                              api_key=config.themoviedb_v3_api_key)
+
+    # Request movie info from TheMovieDB API
+    response = requests.request("GET", trailer_access_url).json()
+    trailer = response.get('results')[0]
+
+    # Return the YouTube key if the trailer is on YouTube
+    # Otherwise error.
+    if(trailer.get('site') == "YouTube"):
+        return trailer.get('key')
+    else:
+        return "Error"
+
+
+def get_movie_list(list_id, movie_array):
+    """
+    Fetches the list provided and creates an array of Movies from it
+
+    Attributes:
+        list_id (str or int): numerical ID number of a list at TheMovieDB.org
+        movie_array (array): an array to hold the Movie objects
+    """
+
+    # Image paths will need this prepended
+    image_url_pre = "https://image.tmdb.org/t/p/w500"
+    list_access_url = (
+        "https://api.themoviedb.org/3/list/{list}?api_key={api_key}"
+        "&language=en-US").format(list=str(list_id),
+                                  api_key=config.themoviedb_v3_api_key)
+
+    # Request list from TheMovieDB API
+    response = requests.request("GET", list_access_url).json()
+    movies = response.get('items')
+
+    # Adds each movie in the list to the array provided
+    for item in movies:
+        movie_array.append(media.Movie(item.get('title'),
+                                       item.get('overview'),
+                                       image_url_pre + item.get('poster_path'),
+                                       get_trailer(item.get('id'))))
