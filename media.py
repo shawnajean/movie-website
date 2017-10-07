f9493837a3b2a477f0dc2794699a6bcd85af783e
Shawna Jean <shawnajean@users.noreply.github.com>
2017-10-06 23:28:51 -0400

    document Movie attributes

--- media.py
+++ media.py
@@ -1,15 +1,16 @@
-
 class Movie():
     """
     This class provides a way to store movie related information
 
     Attributes:
-
-
+        title (string): title of the movie
+        storyline (string): synopsis of the movie's plot
+        poster_image_url (string): URL of the movie's poster
+        trailer_youtube_url (string): URL of the movie's trailer on YouTube
     """
 
-
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
