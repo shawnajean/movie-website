b61177a1b8a841c87c0de7ba62396107de08c705
Shawna Jean <shawnajean@users.noreply.github.com>
2017-10-08 19:01:03 -0400

    add comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,7 +3,9 @@ import movie_api
 import config
 import site_generator
 
+# This creates the movie array, fills it with information from a list on
+# TheMovideDB via the API, and generates the website from that info.
+
 movies = []
 movie_api.get_movie_list(config.themoviedb_list_id, movies)
-
 site_generator.open_movies_page(movies)
